1906942bbf70cc2ae4ffb09df9bbf61c3ab4e4f4
Jyotiprokash Banerjee <jyotib652@gmail.com>
2023-01-09 18:21:27 +0530

    "added makefile and helpers functionality"

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,15 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
-
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
-}
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
@@ -18,8 +11,10 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// out, _ := json.MarshalIndent(payload, "", "/t")
-	out, _ := json.Marshal(payload)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(out)
+	// out, _ := json.Marshal(payload)
+	// w.Header().Set("Content-Type", "application/json")
+	// w.WriteHeader(http.StatusAccepted)
+	// w.Write(out)
+
+	_ = app.writeJSON(w, http.StatusOK, payload)
 }
